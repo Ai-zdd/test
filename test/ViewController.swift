8f8771b15df50c15791a84eab2be4f16c11e2423
Ai-zdd <397608211@qq.com>
2016-08-29 15:32:22 +0800

    test from Xcode

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         // test for me
+        // test argin
     }
 
     override func didReceiveMemoryWarning() {
